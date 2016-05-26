809f1f648e6c97a3fc042148ccd51ad8878e368f
filstojchev <fstojchev@gmail.com>
2016-05-26 15:37:19 +0300

    TorrentPirate

---
+++ TorrentPirate/TorrentPirate/TorrentPirate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+class TorrentPirate
+{
+    static void Main()
+    {
+        double downloadData = double.Parse(Console.ReadLine());
+        double priceOfCinema = double.Parse(Console.ReadLine());
+        double wifeSpending = double.Parse(Console.ReadLine());
+
+        double downloadTime = downloadData / 2 / 60 / 60;
+        double priceForDownload = downloadTime * wifeSpending;
+        double numberOfMovies = downloadData / 1500;
+        double cinemaPrice = numberOfMovies * priceOfCinema;
+
+        if (priceForDownload <= cinemaPrice)
+        {
+            Console.WriteLine("mall -> {0:F2}lv", priceForDownload);
+        }
+        else
+        {
+            Console.WriteLine("cinema -> {0:F2}lv", cinemaPrice);
+        }
+    }
+}
+
